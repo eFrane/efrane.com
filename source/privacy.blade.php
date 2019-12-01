e8c17dbb29d14875b1e5ad06b1f153f237a62534
eFrane <efrane@meanderingsoul.com>
2019-12-01 11:19:58 +0100

    Link styling

--- source/privacy.blade.php
+++ source/privacy.blade.php
@@ -9,9 +9,9 @@
     <h1 class="text-2xl">Privacy Statement</h1>
 
     <p>
-        This website is hosted on a <a href="https://digitalocean.com">Digital Ocean</a>
+        This website is hosted on a <a href="https://digitalocean.com" class="text-blue-700 hover:text-pink-900">Digital Ocean</a>
         droplet in their Frankfurt, Germany Datacenter. All communication is SSL encrypted
-        with a <a href="https://letsencrypt.org">Let's encrypt</a> certificate.
+        with a <a href="https://letsencrypt.org" class="text-blue-700 hover:text-pink-900">Let's encrypt</a> certificate.
         <strong class="font-medium">No logging of any kind is done between the server and
         the web client.</strong>
     </p>
